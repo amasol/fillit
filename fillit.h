5de3e9d8e9dcbde82cdd51467cb851cec99df5f5
Andrii MASOL <amasol@e3r1p2.unit.ua>
2017-12-22 14:17:26 +0200

    1232

---
+++ fillit.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amasol <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/11 14:12:00 by amasol            #+#    #+#             */
+/*   Updated: 2017/12/12 19:55:59 by amasol           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#	ifndef FILLIT_H
+#	define FILLIT_H
+
+#	include <stdio.h>
+#	include <stdlib.h>
+
+void	ft_putstr(char const *str);
+char	read(int argc, char **argv);
+void	ft_putchar(char c);
+
+#	endif
